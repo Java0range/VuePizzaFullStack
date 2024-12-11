359e4ea76e3804f76dcbeb2a4460a60dd27ca83e
JavaOrange <javared234@gmail.com>
2024-12-11 22:25:45 +0300

    fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import { provide, ref } from 'vue'
 
-const serverUrl = ref('http://77.223.99.133')
+const serverUrl = ref('http://77.223.99.133:8000')
 
 axios.defaults.baseURL = serverUrl.value
 
